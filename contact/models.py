e6f4b21dd9da795efdf8d80c6da76ea40f0ef648
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-01-22 16:47:26 -0300

    realizando anotações de código

--- contact/models.py
+++ contact/models.py
@@ -10,7 +10,8 @@ class Contact(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     phone = models.CharField(max_length=50)
-    # blank torna um campo obrigatório ou não.
+    # blank torna um campo obrigatório ou não, porém não afeta na base de dados
+    # EmailField() possui um validador de email
     email = models.EmailField(max_length=254, blank=True)
     # registra de forma automática o momento de criação
     created_date = models.DateTimeField(default=timezone.now)
