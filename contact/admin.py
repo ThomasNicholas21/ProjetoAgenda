2fb34aa3b21aa2163f73ff952b7b183397bfd1e6
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-01-22 16:47:59 -0300

    criando model no painel administrativo do django

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from contact import models
 
 # Register your models here.
+# Configura os models dentro do painel administrativo do Django
+
+# Função decoradora que permite registrar
+@admin.register(models.Contact)
+class ContactAdmin(admin.ModelAdmin): ...
