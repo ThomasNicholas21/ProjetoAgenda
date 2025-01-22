2f29f9c0657e7087ef3d5e8cffc355f5710bb02d
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-01-22 16:12:21 -0300

    criando primeiro model para aplicação

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+# modulo de utilitários
+from django.utils import timezone
 
 # Create your models here.
+
+# O ID é criado de forma automática pelo Django.
+# Model usado para criar o CRUD
+class Contact(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max=50)
+    # blank torna um campo obrigatório ou não.
+    email = models.EmailField(max_length=254, blank=True)
+    # registra de forma automática o momento de criação
+    created_date = models.DateTimeField(default=timezone.now)
+    # TextField() não necessita de definir tamanho de caracter
+    description = models.TextField(blank=True)
