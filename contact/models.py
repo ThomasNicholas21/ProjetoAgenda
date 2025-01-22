37bc61bf3e78031cce7ec0b5f35492c8c8b60aad
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-01-22 16:15:46 -0300

    correção

--- contact/models.py
+++ contact/models.py
@@ -9,7 +9,7 @@ from django.utils import timezone
 class Contact(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    phone = models.CharField(max=50)
+    phone = models.CharField(max_length=50)
     # blank torna um campo obrigatório ou não.
     email = models.EmailField(max_length=254, blank=True)
     # registra de forma automática o momento de criação
