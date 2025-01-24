3ad471332aa29295140cd9f264c38152d092ac00
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-01-24 17:44:09 -0300

    configurando str para category

--- contact/models.py
+++ contact/models.py
@@ -9,6 +9,9 @@ from django.utils import timezone
 class Category(models.Model):
     name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return f'{self.name}'
+
 class Contact(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50, blank=True)
