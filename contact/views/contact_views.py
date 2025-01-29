5d884c7bc665d13ee30017c96756bb1a161aaf00
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-01-28 23:10:48 -0300

    criando view para contato

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -13,3 +13,16 @@ def index(request):
         request,
         'contact/index.html',
         context)
+
+
+def contact(request, contact_id):
+    singles_contact = Contact.objects.get(pk=contact_id)
+
+    context = {
+        'contact': singles_contact, 
+    }
+
+    return render(
+        request,
+        'contact/contact.html',
+        context)
