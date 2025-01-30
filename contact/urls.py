6f3947cee87f689e1c4c00505b035e60ab0fef69
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-01-29 22:55:00 -0300

    separando URLs para criar (create)

--- contact/urls.py
+++ contact/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('search/', views.search, name='search'),
     # CRUD - Read
     path('contact/<int:contact_id>/detail/', views.contact, name='contact'),
+    # CRUD - Create
+    path('contact/<int:contact_id>/create/', views.contact, name='contact'),
 ]
