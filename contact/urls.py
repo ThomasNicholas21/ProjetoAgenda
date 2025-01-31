569ba55e9b579619c3f28350cec1ba8f86608e20
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-01-30 21:38:15 -0300

    fixando apenas a url utilizada no momento

--- contact/urls.py
+++ contact/urls.py
@@ -7,11 +7,11 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('search/', views.search, name='search'),
     # CRUD - Read
-    path('contact/<int:contact_id>/detail/', views.contact, name='contact'),
+    # path('contact/<int:contact_id>/detail/', views.contact, name='contact'),
     # CRUD - Create
-    path('contact/create/', views.create, name='contact'),
+    path('contact/create/', views.create, name='create'),
     # CRUD - UPDATE
-    path('contact/<int:contact_id>/update/', views.contact, name='contact'),
+    # path('contact/<int:contact_id>/update/', views.contact, name='contact'),
     # CRUD - DELETE
-    path('contact/<int:contact_id>/delete/', views.contact, name='contact'),
+    # path('contact/<int:contact_id>/delete/', views.contact, name='contact'),
 ]
