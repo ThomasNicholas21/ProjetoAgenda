4b4843505dc2e2ba896b3380fa449404e21c90e4
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-01-31 17:06:46 -0300

    reorganizando views de forms

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,28 +1,5 @@
 from django.shortcuts import render
-from django.core.paginator import Paginator
-from contact.models import Contact
-from django import forms
-from django.core.exceptions import ValidationError
-
-class ContactForm(forms.ModelForm):
-    class Meta: 
-        # define qual model esse form se refere
-        model = Contact
-        fields = ('first_name', 'last_name', 'phone',)
-
-    # pega os dados do formulário
-    def clean(self):
-        cleaned_data = self.cleaned_data
-        
-        self.add_error(
-            'first_name',
-            ValidationError(
-                'Erro',
-                code='invalid'
-            )
-        )
-
-        return super().clean()
+from contact.forms import ContactForm
 
 def create(request):
     if request.method == "POST":
