0139140b2b932459a0aa1c4028b4a5dcb05525ea
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-02-01 13:52:57 -0300

    configurando widgets, label e help_text

--- contact/forms.py
+++ contact/forms.py
@@ -3,19 +3,29 @@ from django.core.exceptions import ValidationError
 from contact.models import Contact
 
 class ContactForm(forms.ModelForm):
+    first_name = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'placeholder':'Escreva seu nome'
+            }
+        ),
+        label='Primeiro Nome',
+        help_text='Texto de ajuda.'
+    )
+
     class Meta: 
         # define qual model esse form se refere
         model = Contact
         fields = ('first_name', 'last_name', 'phone',)
 
         # permite definir atributos dentro de tags do forms
-        widgets = {
-            'first_name': forms.TextInput(
-                attrs={
-                    'placeholder':'Escreva seu nome'
-                }
-            )
-        }
+        # widgets = {
+        #     'first_name': forms.TextInput(
+        #         attrs={
+        #             'placeholder':'Escreva seu nome'
+        #         }
+        #     )
+        # }
     # pega os dados do formulário
     def clean(self):
         cleaned_data = self.cleaned_data
