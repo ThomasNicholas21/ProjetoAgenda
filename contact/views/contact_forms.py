8b57f3093a5a5e284ca85b6a8f566459a5a20f4f
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-02-03 20:44:58 -0300

    replicando view para comecar a url de update

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -37,3 +37,38 @@ def create(request):
         context
         )
 
+
+def update(request):
+    form_action = reverse('contact:create')
+
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        context = {
+            'form': form,
+            'form_action': form_action
+        }
+
+        # funcao que retorna true se o formulario
+        # não retornar nenhum erro
+        if form.is_valid(): 
+            contact = form.save()
+            return redirect('contact:update', contact_id=contact.pk)
+
+
+        return render(
+                request,
+                'contact/create.html',
+                context
+                )
+
+    form = ContactForm()
+    context = {
+        'form': form,
+        'form_action': form_action
+    }
+
+    return render(
+        request,
+        'contact/create.html',
+        context
+        )
