bb3211ed1e5a865e2f95db8457534dd6d5f14a43
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-02-04 23:10:32 -0300

    criando view para ciracao de usuario

---
+++ contact/views/user_forms.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from contact.forms import RegisterForm
+
+def register(request):
+    form = RegisterForm()
+
+    context = {
+        'form': form
+    }
+    
+    return render(
+        request,
+        'contact/register.html',
+        context
+    )
