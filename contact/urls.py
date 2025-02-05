06dcda70b2c6da1fad08b1703eeff68583d7f8ed
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-02-04 23:10:17 -0300

    criando url de criacao de usuario

--- contact/urls.py
+++ contact/urls.py
@@ -6,13 +6,21 @@ app_name = 'contact'
 urlpatterns = [
     path('', views.index, name='index'),
     path('search/', views.search, name='search'),
-    path('contact/<int:contact_id>/', views.contact, name='contact'),
+    # CRUD - Contact
     # CRUD - Read
-    # path('contact/<int:contact_id>/detail/', views.contact, name='contact'),
+    path('contact/<int:contact_id>/', views.contact, name='contact'),
     # CRUD - Create
     path('contact/create/', views.create, name='create'),
     # CRUD - UPDATE
     path('contact/<int:contact_id>/update/', views.update, name='update'),
     # CRUD - DELETE
     path('contact/<int:contact_id>/delete/', views.delete, name='delete'),
+
+    # CRU - USER
+    # CRUD - Read
+    #path('contact/<int:contact_id>/', views.contact, name='contact'),
+    # CRUD - Create
+    path('user/create/', views.register, name='register'),
+    # CRUD - UPDATE
+    #path('contact/<int:contact_id>/update/', views.update, name='update'),
 ]
