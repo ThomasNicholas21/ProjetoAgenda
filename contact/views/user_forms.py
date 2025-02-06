f2ff84e9e0a75e1123b4430384b069435643625d
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-02-05 23:04:15 -0300

    login_view

--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.urls import reverse
 from contact.forms import RegisterForm
 from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
 
 def register(request):
     form_action = reverse('contact:register')
@@ -21,4 +22,23 @@ def register(request):
             'form': RegisterForm(),
             'form_action': form_action,
         }
+    )
+
+def login_view(request):
+    form = AuthenticationForm(request)
+
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+
+        if form.is_valid():
+            user = form.get_user()
+    
+
+    context = {
+        'form': form
+    }
+    return render(
+        request,
+        'contact/login.html',
+        context
     )
