a24308b40d749dc5a91c84eb90db132a3b4fc87a
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-02-06 21:50:54 -0300

    criando forms de update

--- contact/forms.py
+++ contact/forms.py
@@ -165,3 +165,12 @@ class RegisterForm(UserCreationForm):
             )
 
         return email
+    
+class RegisterUpdteForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = (
+            'first_name', 'last_name', 
+            'email', 'username', 
+            # 'password1', 'password2'
+        )
