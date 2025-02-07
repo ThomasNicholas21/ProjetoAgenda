6e8f99d65f5d28f0a57c0cd2739cfcbbb1695d6b
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-02-06 23:09:24 -0300

    criando commit de senha para o banco de dados

--- contact/forms.py
+++ contact/forms.py
@@ -213,6 +213,17 @@ class RegisterUpdteForm(forms.ModelForm):
             # 'password1', 'password2'
         )
 
+    def save(self, commit=True):
+        cleaned_data = self.cleaned_data
+        user =  super().save(commit=False)
+
+        password = cleaned_data.get('password1')
+        if password:
+            user.set_password(password)
+
+        if commit:
+            user.save()
+
     def clean(self):
         password1 = self.cleaned_data.get('password1')
         password2 = self.cleaned_data.get('password2')
