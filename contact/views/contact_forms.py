3e64cebe929a4f97ea7dda8754d29c1c8a2676bd
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-02-10 18:39:19 -0300

    criando update para delete

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -87,6 +87,7 @@ def update(request, contact_id):
         context
         )
 
+@login_required(login_url='contact:login')
 def delete(request, contact_id):
     contact = get_object_or_404(
         Contact, pk=contact_id, show=True
