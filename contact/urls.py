d9b1a379aa422e6dfb685fb8a69d4528c97a5952
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-02-10 17:38:12 -0300

    verificando quais urls necessitam de permissao de acesso

--- contact/urls.py
+++ contact/urls.py
@@ -9,11 +9,11 @@ urlpatterns = [
     # CRUD - Contact
     # CRUD - Read
     path('contact/<int:contact_id>/', views.contact, name='contact'),
-    # CRUD - Create
+    # CRUD - Create - Login Required
     path('contact/create/', views.create, name='create'),
-    # CRUD - UPDATE
+    # CRUD - UPDATE - Login Required
     path('contact/<int:contact_id>/update/', views.update, name='update'),
-    # CRUD - DELETE
+    # CRUD - DELETE - Login Required
     path('contact/<int:contact_id>/delete/', views.delete, name='delete'),
 
     # CRU - USER
@@ -21,9 +21,10 @@ urlpatterns = [
     #path('contact/<int:contact_id>/', views.contact, name='contact'),
     # CRUD - Create
     path('user/create/', views.register, name='register'),
-    # CRUD - UPDATE
     #path('contact/<int:contact_id>/update/', views.update, name='update'),
     path('user/login/', views.login_view, name='login'),
+    # Login Required
     path('user/logout/', views.logout_view, name='logout'),
+    # CRUD - UPDATE - Login Required
     path('user/update/', views.update_view, name='user_update'),
 ]
